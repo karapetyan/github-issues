69b9d2c95a979f902701a9931dcd2905355ca347
Sergey Karapetyan <grey.karapetyan@gmail.com>
2018-08-01 17:33:55 +0300

    fix dev-tools

--- src/store/store.js
+++ src/store/store.js
@@ -3,10 +3,13 @@ import thunk from 'redux-thunk';
 
 import rootReducer from '../reducers/index';
 
+
+
 export const store = createStore(
     rootReducer,
     compose(
         applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-      )
+        window.navigator.userAgent.includes('Chrome') ?
+            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : compose,
+    )
 );
